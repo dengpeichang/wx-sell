775afb688516b294a1c9bc60ccee1c50e5fb9b0b
dengpeichang <dpc1402830814@126.com>
2018-08-20 19:48:39 +0800

    将jpa改为mybatics

---
+++ src/main/java/com/imooc/wxsell/mapper/OrderMasterMapper.java
@@ -0,0 +1,7 @@
+package com.imooc.wxsell.mapper;
+
+import com.imooc.wxsell.config.MyMapper;
+import com.imooc.wxsell.dataobject.OrderMaster;
+
+public interface OrderMasterMapper extends MyMapper<OrderMaster> {
+}
